2719ba727f13316fee6e3f237f334d67e0d49028
“joshbav” <joshbav@users.noreply.github.com>
2021-06-12 16:13:31 -0700

    Scripted commit 12-Jun-2021-04:13:31PM

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ echo Pushing newly built image to dockerhub
 echo
 docker push joshbav/ubuntu:latest
 
-git config user.name “joshbav”
+echo
+echo
+echo Adding all files in repo and pushing to git
+git config user.name “joshbav” --local
+git config user.email "joshbav@users.noreply.github.com" --local
 git add -A
 git commit -m "Scripted commit $(date +%d-%b-%Y-%I:%M:%S%p)"
 git push -u origin master
